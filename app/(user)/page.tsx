4cbeb9e87262a7664e8ebac50640bc6b07600f39
Joe Alt <joejoyalt@gmail.com>
2023-08-12 15:02:18 +0200

    initial commit

---
+++ app/(user)/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <main className="flex items-center justify-center gap-8 p-24">
+        <div className="z-10 max-w-5xl items-center font-mono text-sm lg:flex">
+          <h1>Caps & Hats</h1>
+        </div>
+        <p className="text-xl">Hello There</p>
+      </main>
+    </>
+  );
+}
